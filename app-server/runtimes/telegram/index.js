467d4aa8b35787f50898a94d8c90ff8fe8e42ef0
Renzil DSouza <renzil@gmail.com>
2021-04-27 16:28:38 +0530

    Mockabot first-cut:
    - /connect to cache a bot token and use the bot username
    - /send to send a message as another bot, to any chat id
    - For rich-text and multimedia messages, create a group with Mockabot and the target bot and make them both admins; then send any rich-text / multimedia message and reply to the message with /send
    - Support for sending most message types as another bot through above method

--- app-server/runtimes/telegram/index.js
+++ app-server/runtimes/telegram/index.js
@@ -2,9 +2,10 @@ const queue = require('async/queue');
 const { get: getObjectProperty } = require('lodash/object');
 
 const logger = require('../../logger');
-const { sendMessage, leaveChat } = require('../../utils/telegram');
+const { TELEGRAM_MESSAGE_TYPES, getMe, sendMessage, leaveChat, cloneMessage } = require('../../utils/telegram');
 
 const bots = {};
+const connected_bots = {};
 
 function addBot(username, secret, callbacks) {
   bots[username] = {
@@ -19,13 +20,18 @@ function getChatQueue(botUsername, botSecret, chatId) {
     (bots[botUsername].queue[chatId] || (bots[botUsername].queue[chatId] = queue(processUpdate, 1)));
 }
 
+function hasAnyKey(object, baseKey, keys) {
+  return keys.some(key => getObjectProperty(object, `${baseKey}.${key}`) !== undefined);
+}
+
 async function processUpdate(task, callback) {
   try {
     const chat_type = getObjectProperty(task, "update.message.chat.type") ||
       getObjectProperty(task, "update.my_chat_member.chat.type") ||
       getObjectProperty(task, "update.channel_post.chat.type");
+
     const chat_text = getObjectProperty(task, "update.message.text");
-    const channel_text = getObjectProperty(task, "update.channel_post.text");
+
     const new_chat_members = getObjectProperty(task, "update.message.new_chat_members");
     const my_chat_member_new_status = getObjectProperty(task, "update.my_chat_member.new_chat_member.status");
     switch (chat_type) {
@@ -34,7 +40,7 @@ async function processUpdate(task, callback) {
           await bots[task.botUsername].callbacks.onPMChatJoin(task.update);
         } else if (my_chat_member_new_status === "kicked") {
           await bots[task.botUsername].callbacks.onPMChatBlocked(task.update);
-        } else if (chat_text) {
+        } else if (hasAnyKey(task, "update.message", TELEGRAM_MESSAGE_TYPES)) {
           await bots[task.botUsername].callbacks.onPMChatMessage(task.update);
         }
         break;
@@ -45,7 +51,7 @@ async function processUpdate(task, callback) {
           await bots[task.botUsername].callbacks.onGroupChatJoin(task.update);
         } else if (my_chat_member_new_status === "left") {
           await bots[task.botUsername].callbacks.onGroupChatLeave(task.update);
-        } else if (chat_text) {
+        } else if (hasAnyKey(task, "update.message", TELEGRAM_MESSAGE_TYPES)) {
           await bots[task.botUsername].callbacks.onGroupChatMessage(task.update);
         }
         break;
@@ -55,7 +61,7 @@ async function processUpdate(task, callback) {
           await bots[task.botUsername].callbacks.onChannelJoin(task.update);
         } else if (my_chat_member_new_status === "left") {
           await bots[task.botUsername].callbacks.onChannelLeave(task.update);
-        } else if (channel_text) {
+        } else if (hasAnyKey(task, "update.channel_post", TELEGRAM_MESSAGE_TYPES)) {
           await bots[task.botUsername].callbacks.onChannelMessage(task.update);
         }
         break;
@@ -65,6 +71,7 @@ async function processUpdate(task, callback) {
     }
 
   } catch (err) {
+    logger.error(err);
     if (callback) {
       callback(err);
     }
@@ -75,35 +82,117 @@ addBot(process.env.TELEGRAM_BOT_USERNAME, process.env.TELEGRAM_BOT_SECRET, {
   onPMChatJoin: async function (update) {
     await sendMessage({
       chat_id: update.message.chat.id,
-      text: `Hi ${update.message.from.username ? '@' + update.message.from.username : update.message.from.first_name}`,
+      text: `Hi ${update.message.from.first_name}! My name is Mockabot - I can help you mock the experience of a Telegram chatbot.
+      
+To begin, use @BotFather to create a new bot. Once you have the bot token, you can send a message as your bot to anyone using the command \`/send <BOT TOKEN> <USERNAME> <MESSAGE>\`. Before sending a message, your bot must have initiated a conversation with \`<USERNAME>\`. 
+      
+If you'd like to not send the bot token every time, I can cache it for you with the command \`/connect <BOT TOKEN>\`. After this you can simply use \`/send <BOT USERNAME> <USERNAME> <MESSAGE>\`.
+
+If you want to send a media or formatted text message, you can just send me the message you want to send and then reply to it with \`/send <BOT USERNAME> <USERNAME>\`.
+
+You can even send to a group using \`/send <BOT USERNAME> <GROUPNAME>\`. Use \`/send <BOT USERNAME> <CHAT ID>\` to send to a private group, where chat id is obtained by sending /chatid in the private group.
+      
+Click /help for details on my commands and their usage.`,
+      parse_mode: "Markdown",
     },
       process.env.TELEGRAM_BOT_TOKEN);
     logger.info(`@${process.env.TELEGRAM_BOT_USERNAME} started PM chat with ${update.message.from.first_name} | ${update.message.from.username} | ${update.message.from.id} `);
   },
   onPMChatMessage: async function (update) {
-    await sendMessage({
-      chat_id: update.message.chat.id,
-      text: update.message.text,
-    },
-      process.env.TELEGRAM_BOT_TOKEN);
+    if (!update.message.text) {
+      return;
+    }
+    let apiResponse;
+    const args = update.message.text.split(' ');
+    if (!args.length) {
+      return;
+    }
+    switch (args[0]) {
+      case "/help":
+        apiResponse = await sendMessage({
+          chat_id: update.message.chat.id,
+          text: `Working on it, I will update this soon.`,
+        },
+          process.env.TELEGRAM_BOT_TOKEN);
+        break;
+      case "/chatid":
+        apiResponse = await sendMessage({
+          chat_id: update.message.chat.id,
+          text: `${update.message.chat.id}`,
+          reply_to_message_id: update.message.message_id,
+        },
+          process.env.TELEGRAM_BOT_TOKEN);
+        break;
+      case "/connect":
+        {
+          if (args[1] === undefined) {
+            await sendMessage({
+              chat_id: update.message.chat.id,
+              text: `Usage - \`/connect <BOT TOKEN>\``,
+              parse_mode: "Markdown",
+              reply_to_message_id: update.message.message_id,
+            },
+              process.env.TELEGRAM_BOT_TOKEN);
+            return;
+          }
+          const bot_token = args[1];
+          try {
+            apiResponse = await getMe(bot_token);
+            const bot_username = `@${apiResponse.data.result.username}`;
+            connected_bots[bot_username] = bot_token;
+            await sendMessage({
+              chat_id: update.message.chat.id,
+              text: `Connected bot ${bot_username}. You can now use the bot's username instead of the token for all requests.`,
+              reply_to_message_id: update.message.message_id,
+            },
+              process.env.TELEGRAM_BOT_TOKEN);
+          } catch (err) {
+            await sendMessage({
+              chat_id: update.message.chat.id,
+              text: `Unable to connect this token. Please check the token and try again.`,
+              reply_to_message_id: update.message.message_id,
+            },
+              process.env.TELEGRAM_BOT_TOKEN);
+          }
+        }
+        break;
+      case "/send":
+        {
+          command_send(update, args);
+        }
+        break;
+    }
   },
   onPMChatBlocked: async function (update) {
     logger.info(`@${process.env.TELEGRAM_BOT_USERNAME} blocked by ${update.my_chat_member.from.first_name} | ${update.my_chat_member.from.username} | ${update.my_chat_member.from.id}`);
   },
   onGroupChatJoin: async function (update) {
-    await sendMessage({
-      chat_id: update.message.chat.id,
-      text: `Hi ${update.message.from.username ? '@' + update.message.from.username : update.message.chat.title}!`,
-    },
-      process.env.TELEGRAM_BOT_TOKEN);
     logger.info(`@${process.env.TELEGRAM_BOT_USERNAME} joined group ${update.message.chat.title} | ${update.message.chat.id} by ${update.message.from.first_name} | ${update.message.from.username} | ${update.message.from.id} `);
   },
   onGroupChatMessage: async function (update) {
-    await sendMessage({
-      chat_id: update.message.chat.id,
-      text: update.message.text,
-    },
-      process.env.TELEGRAM_BOT_TOKEN);
+    if (!update.message.text) {
+      return;
+    }
+    let apiResponse;
+    const args = update.message.text.split(' ');
+    if (!args.length) {
+      return;
+    }
+    switch (args[0]) {
+      case "/chatid":
+        apiResponse = await sendMessage({
+          chat_id: update.message.chat.id,
+          text: `${update.message.chat.id}`,
+          reply_to_message_id: update.message.message_id,
+        },
+          process.env.TELEGRAM_BOT_TOKEN);
+        break;
+      case "/send":
+        {
+          await command_send(update, args);
+        }
+        break;
+    }
   },
   onGroupChatLeave: async function (update) {
     logger.info(`@${process.env.TELEGRAM_BOT_USERNAME} kicked in group ${update.my_chat_member.chat.title} | ${update.my_chat_member.chat.id} by ${update.my_chat_member.from.first_name} | ${update.my_chat_member.from.username} | ${update.my_chat_member.from.id} `);
@@ -121,6 +210,94 @@ addBot(process.env.TELEGRAM_BOT_USERNAME, process.env.TELEGRAM_BOT_SECRET, {
     logger.info(`@${process.env.TELEGRAM_BOT_USERNAME} kicked in channel ${update.my_chat_member.chat.title} | ${update.my_chat_member.chat.id} by ${update.my_chat_member.from.first_name} | ${update.my_chat_member.from.username} | ${update.my_chat_member.from.id} `);
   },
 });
+
+async function command_send(update, args) {
+  let apiResponse;
+  if (args[1] === undefined || args[2] === undefined || (!update.message.reply_to_message && args[3] === undefined)) {
+    await sendMessage({
+      chat_id: update.message.chat.id,
+      text: `Usage - \`/send <BOT TOKEN | BOT USERNAME> <CHAT ID | GROUP USERNAME>${update.message.reply_to_message ? '' : ' <MESSAGE>'}\``,
+      parse_mode: "Markdown",
+      reply_to_message_id: update.message.message_id,
+    },
+      process.env.TELEGRAM_BOT_TOKEN);
+    return;
+  }
+
+  let bot_token = args[1];
+  const to_chat_id = args[2];
+  const message = args[3];
+  let reply_markup = { inline_keyboard: [] };
+
+  let reply_markup_text = update.message.text.match(/\[.+]/g);
+  if (reply_markup_text && reply_markup_text.length) {
+    reply_markup_text = reply_markup_text[0];
+  }
+
+  if (reply_markup_text) {
+    try {
+      const button_rows = reply_markup_text.match(/\[[^\[\]]+]/gm);
+      button_rows.forEach(row => {
+        const reply_markup_row = [];
+        const columns = row.slice(1, -1).trim().split(',');
+        columns.forEach(column => {
+          reply_markup_row.push({
+            text: column,
+            callback_data: column,
+          });
+        });
+        reply_markup.inline_keyboard.push(reply_markup_row);
+      });
+    } catch (err) {
+      logger.error(err);
+    }
+  }
+
+  if (bot_token.charAt(0) === "@") {
+    token = connected_bots[bot_token];
+    if (!token) {
+      apiResponse = await sendMessage({
+        chat_id: update.message.chat.id,
+        text: `Bot ${bot_token} is not connected. Please use \`/connect <BOT_TOKEN>\` to connect this bot first.`,
+        parse_mode: "Markdown",
+        reply_to_message_id: update.message.message_id,
+      },
+        process.env.TELEGRAM_BOT_TOKEN);
+      return;
+    }
+    bot_token = token;
+  }
+  try {
+    if (update.message.reply_to_message) {
+      apiResponse = await cloneMessage({
+        chat_id: to_chat_id,
+        message: update.message.reply_to_message,
+        reply_markup,
+      },
+        bot_token);
+    } else {
+      apiResponse = await sendMessage({
+        chat_id: to_chat_id,
+        text: message,
+        reply_markup,
+      },
+        bot_token);
+    }
+  } catch (err) {
+    if (err.response) {
+      apiResponse = err.response;
+    } else {
+      throw err;
+    }
+  }
+  const response = apiResponse.status === 200 ? "✅ Message sent" : "❌ Message not sent";
+  apiResponse = await sendMessage({
+    chat_id: update.message.chat.id,
+    text: response,
+    reply_to_message_id: update.message.message_id,
+  },
+    process.env.TELEGRAM_BOT_TOKEN);
+}
 
 module.exports = {
   getChatQueue,
--- app-server/utils/telegram.js
+++ app-server/utils/telegram.js
@@ -1,4 +1,7 @@
 const axios = require('axios').default;
+const { get: getObjectProperty } = require('lodash/object');
+
+const TELEGRAM_MESSAGE_TYPES = ["text", "animation", "audio", "document", "photo", "sticker", "video", "video_note", "voice", "caption", "contact", "dice", "game", "poll", "venue", "location"];
 
 async function callTelegramApi(endpoint, token, body = {}) {
   return axios.post(`https://api.telegram.org/bot${token}/${endpoint}`, {
@@ -10,7 +13,14 @@ function getWebhookUrl(hostUrl, username, secret) {
   return `${hostUrl}/webhooks/telegram/${username}/${secret}`;
 }
 
-async function sendMessage({ chat_id, text, reply_to_message_id = "", reply_markup = {} }, botToken) {
+async function getMe(botToken) {
+  return callTelegramApi(
+    'getMe',
+    botToken,
+  );
+}
+
+async function sendMessage({ chat_id, text, reply_to_message_id = "", reply_markup = {}, parse_mode = "", entities = [] }, botToken) {
   return callTelegramApi(
     'sendMessage',
     botToken,
@@ -19,6 +29,8 @@ async function sendMessage({ chat_id, text, reply_to_message_id = "", reply_mark
       text,
       reply_to_message_id,
       reply_markup,
+      parse_mode,
+      entities,
     },
   );
 }
@@ -48,6 +60,235 @@ async function copyMessage({ chat_id, from_chat_id, message_id, reply_to_message
   );
 }
 
+async function sendPhoto({ chat_id, photo, caption, caption_entities, reply_to_message_id = "", reply_markup = {}, parse_mode = "" }, botToken) {
+  return callTelegramApi(
+    'sendPhoto',
+    botToken,
+    {
+      chat_id,
+      photo,
+      caption,
+      caption_entities,
+      reply_to_message_id,
+      reply_markup,
+      parse_mode,
+    },
+  );
+}
+
+async function sendAudio({ chat_id, audio, duration, performer, title, thumb, caption, caption_entities, reply_to_message_id = "", reply_markup = {}, parse_mode = "" }, botToken) {
+  return callTelegramApi(
+    'sendAudio',
+    botToken,
+    {
+      chat_id,
+      audio,
+      duration,
+      performer,
+      title,
+      thumb,
+      caption,
+      caption_entities,
+      reply_to_message_id,
+      reply_markup,
+      parse_mode,
+    },
+  );
+}
+
+async function sendDocument({ chat_id, document, thumb, caption, caption_entities, reply_to_message_id = "", reply_markup = {}, parse_mode = "" }, botToken) {
+  return callTelegramApi(
+    'sendDocument',
+    botToken,
+    {
+      chat_id,
+      document,
+      thumb,
+      caption,
+      caption_entities,
+      reply_to_message_id,
+      reply_markup,
+      parse_mode,
+    },
+  );
+}
+
+async function sendVideo({ chat_id, video, duration, width, height, thumb, caption, caption_entities, reply_to_message_id = "", reply_markup = {}, parse_mode = "" }, botToken) {
+  return callTelegramApi(
+    'sendVideo',
+    botToken,
+    {
+      chat_id,
+      video,
+      duration,
+      width,
+      height,
+      thumb,
+      caption,
+      caption_entities,
+      reply_to_message_id,
+      reply_markup,
+      parse_mode,
+    },
+  );
+}
+
+async function sendAnimation({ chat_id, animation, duration, width, height, thumb, caption, caption_entities, reply_to_message_id = "", reply_markup = {}, parse_mode = "" }, botToken) {
+  return callTelegramApi(
+    'sendAnimation',
+    botToken,
+    {
+      chat_id,
+      animation,
+      duration,
+      width,
+      height,
+      thumb,
+      caption,
+      caption_entities,
+      reply_to_message_id,
+      reply_markup,
+      parse_mode,
+    },
+  );
+}
+
+async function sendVoice({ chat_id, voice, duration, caption, caption_entities, reply_to_message_id = "", reply_markup = {}, parse_mode = "" }, botToken) {
+  return callTelegramApi(
+    'sendVoice',
+    botToken,
+    {
+      chat_id,
+      voice,
+      duration,
+      caption,
+      caption_entities,
+      reply_to_message_id,
+      reply_markup,
+      parse_mode,
+    },
+  );
+}
+
+async function sendVideoNote({ chat_id, video_note, duration, length, thumb, reply_to_message_id = "", reply_markup = {} }, botToken) {
+  return callTelegramApi(
+    'sendVideoNote',
+    botToken,
+    {
+      chat_id,
+      video_note,
+      duration,
+      length,
+      thumb,
+      reply_to_message_id,
+      reply_markup,
+    },
+  );
+}
+
+async function sendLocation({ chat_id, longitude, latitude, horizontal_accuracy, live_period, heading, proximity_alert_radius, reply_to_message_id = "", reply_markup = {} }, botToken) {
+  return callTelegramApi(
+    'sendLocation',
+    botToken,
+    {
+      chat_id,
+      longitude,
+      latitude,
+      horizontal_accuracy,
+      live_period,
+      heading,
+      proximity_alert_radius,
+      reply_to_message_id,
+      reply_markup,
+    },
+  );
+}
+
+async function sendVenue({ chat_id, longitude, latitude, title, address, foursquare_id, foursquare_type, google_place_id, google_place_type, reply_to_message_id = "", reply_markup = {} }, botToken) {
+  return callTelegramApi(
+    'sendVenue',
+    botToken,
+    {
+      chat_id,
+      longitude,
+      latitude,
+      title,
+      address,
+      foursquare_id,
+      foursquare_type,
+      google_place_id,
+      google_place_type,
+      reply_to_message_id,
+      reply_markup,
+    },
+  );
+}
+
+async function sendContact({ chat_id, phone_number, first_name, last_name, vcard, reply_to_message_id = "", reply_markup = {} }, botToken) {
+  return callTelegramApi(
+    'sendContact',
+    botToken,
+    {
+      chat_id,
+      phone_number,
+      first_name,
+      last_name,
+      vcard,
+      reply_to_message_id,
+      reply_markup,
+    },
+  );
+}
+
+async function sendPoll({ chat_id, question, options, is_anonymous, type, allows_multiple_answers, correct_option_id, explanation, explanation_entities, open_period, close_date, is_closed, reply_to_message_id = "", reply_markup = {} }, botToken) {
+  return callTelegramApi(
+    'sendPoll',
+    botToken,
+    {
+      chat_id,
+      question,
+      options,
+      is_anonymous,
+      type,
+      allows_multiple_answers,
+      correct_option_id,
+      explanation,
+      explanation_entities,
+      open_period,
+      close_date,
+      is_closed,
+      reply_to_message_id,
+      reply_markup,
+    },
+  );
+}
+
+async function sendDice({ chat_id, emoji = "", reply_to_message_id = "", reply_markup = {} }, botToken) {
+  return callTelegramApi(
+    'sendDice',
+    botToken,
+    {
+      chat_id,
+      emoji,
+      reply_to_message_id,
+      reply_markup,
+    },
+  );
+}
+
+async function sendSticker({ chat_id, sticker, reply_to_message_id = "", reply_markup = {} }, botToken) {
+  return callTelegramApi(
+    'sendSticker',
+    botToken,
+    {
+      chat_id,
+      sticker,
+      reply_to_message_id,
+      reply_markup,
+    },
+  );
+}
+
 async function leaveChat({ chat_id }, botToken) {
   return callTelegramApi(
     'leaveChat',
@@ -58,11 +299,227 @@ async function leaveChat({ chat_id }, botToken) {
   );
 }
 
+async function cloneMessage({ message, chat_id, reply_to_message_id = "", reply_markup = {} }, botToken) {
+  let apiResponse;
+  TELEGRAM_MESSAGE_TYPES.forEach(type => {
+    if (!message[type] || apiResponse) {
+      return;
+    }
+    switch (type) {
+      case "text":
+        apiResponse = sendMessage({
+          chat_id,
+          reply_to_message_id,
+          reply_markup,
+          text: message.text,
+          entities: message.entities,
+        }, botToken);
+        break;
+
+      case "photo":
+        apiResponse = sendPhoto({
+          chat_id,
+          reply_to_message_id,
+          reply_markup,
+          photo: message.photo[message.photo.length - 1].file_id, // last file observed to be the best quality full-res file
+          caption: message.caption,
+          caption_entities: message.caption_entities,
+        }, botToken);
+        break;
+
+      case "audio":
+        apiResponse = sendAudio({
+          chat_id,
+          reply_to_message_id,
+          reply_markup,
+          audio: message.audio.file_id,
+          duration: message.audio.duration,
+          performer: message.audio.performer,
+          title: message.audio.title,
+          thumb: getObjectProperty(message, "audio.thumb.file_id"),
+          caption: message.caption,
+          caption_entities: message.caption_entities,
+        }, botToken);
+        break;
+
+      case "document":
+        apiResponse = sendDocument({
+          chat_id,
+          reply_to_message_id,
+          reply_markup,
+          document: message.document.file_id,
+          thumb: getObjectProperty(message, "document.thumb.file_id"),
+          caption: message.caption,
+          caption_entities: message.caption_entities,
+        }, botToken);
+        break;
+
+      case "video":
+        apiResponse = sendVideo({
+          chat_id,
+          reply_to_message_id,
+          reply_markup,
+          video: message.video.file_id,
+          duration: message.video.duration,
+          width: message.video.width,
+          height: message.video.height,
+          thumb: getObjectProperty(message, "video.thumb.file_id"),
+          caption: message.caption,
+          caption_entities: message.caption_entities,
+        }, botToken);
+        break;
+
+      case "animation":
+        apiResponse = sendAnimation({
+          chat_id,
+          reply_to_message_id,
+          reply_markup,
+          animation: message.animation.file_id,
+          duration: message.animation.duration,
+          width: message.animation.width,
+          height: message.animation.height,
+          thumb: getObjectProperty(message, "animation.thumb.file_id"),
+          caption: message.caption,
+          caption_entities: message.caption_entities,
+        }, botToken);
+        break;
+
+      case "voice":
+        apiResponse = sendVoice({
+          chat_id,
+          reply_to_message_id,
+          reply_markup,
+          voice: message.voice.file_id,
+          duration: message.voice.duration,
+          caption: message.caption,
+          caption_entities: message.caption_entities,
+        }, botToken);
+        break;
+
+      case "video_note":
+        apiResponse = sendVideoNote({
+          chat_id,
+          reply_to_message_id,
+          reply_markup,
+          video_note: message.video_note.file_id,
+          duration: message.video_note.duration,
+          length: message.video_note.length,
+          thumb: getObjectProperty(message, "video_note.thumb.file_id"),
+        }, botToken);
+        break;
+
+      case "location":
+        apiResponse = sendLocation({
+          chat_id,
+          reply_to_message_id,
+          reply_markup,
+          latitude: message.location.latitude,
+          longitude: message.location.longitude,
+          horizontal_accuracy: message.location.horizontal_accuracy,
+          live_period: message.location.live_period,
+          heading: message.location.heading,
+          proximity_alert_radius: message.location.proximity_alert_radius,
+        }, botToken);
+        break;
+
+      case "venue":
+        apiResponse = sendVenue({
+          chat_id,
+          reply_to_message_id,
+          reply_markup,
+          latitude: message.venue.location.latitude,
+          longitude: message.venue.location.longitude,
+          title: message.venue.title,
+          address: message.venue.address,
+          foursquare_id: message.venue.foursquare_id,
+          foursquare_type: message.venue.foursquare_type,
+          google_place_id: message.venue.google_place_id,
+          google_place_type: message.venue.google_place_type,
+        }, botToken);
+        break;
+
+      case "contact":
+        apiResponse = sendContact({
+          chat_id,
+          reply_to_message_id,
+          reply_markup,
+          phone_number: message.contact.phone_number,
+          first_name: message.contact.first_name,
+          last_name: message.contact.last_name,
+          vcard: message.contact.vcard,
+        }, botToken);
+        break;
+
+      case "poll":
+        apiResponse = sendPoll({
+          chat_id,
+          reply_to_message_id,
+          reply_markup,
+          question: message.poll.question,
+          options: message.poll.options,
+          is_anonymous: message.poll.is_anonymous,
+          type: message.poll.type,
+          allows_multiple_answers: message.poll.allows_multiple_answers,
+          correct_option_id: message.poll.correct_option_id,
+          explanation: message.poll.explanation,
+          explanation_entities: message.poll.explanation_entities,
+          open_period: message.poll.open_period,
+          close_date: message.poll.close_date,
+          is_closed: message.poll.is_closed,
+        }, botToken);
+        break;
+
+      case "dice":
+        apiResponse = sendDice({
+          chat_id,
+          reply_to_message_id,
+          reply_markup,
+          emoji: message.dice.emoji,
+        }, botToken);
+        break;
+
+      case "sticker":
+        apiResponse = sendSticker({
+          chat_id,
+          reply_to_message_id,
+          reply_markup,
+          sticker: message.sticker.file_id,
+        }, botToken);
+        break;
+
+      default:
+        logger.error(`cloneMessage: message type '${type}' not implemented`);
+        break;
+      /*
+      case "game":
+        break;
+      */
+    }
+  });
+  return apiResponse;
+}
+
 module.exports = {
   callTelegramApi,
+  TELEGRAM_MESSAGE_TYPES,
   getWebhookUrl,
+  getMe,
   sendMessage,
   forwardMessage,
   copyMessage,
+  sendPhoto,
+  sendAudio,
+  sendDocument,
+  sendVideo,
+  sendAnimation,
+  sendVoice,
+  sendVideoNote,
+  sendLocation,
+  sendVenue,
+  sendContact,
+  sendPoll,
+  sendDice,
+  sendSticker,
   leaveChat,
+  cloneMessage,
 };
